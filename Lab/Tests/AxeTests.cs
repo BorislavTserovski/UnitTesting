be1d553e86d4a11b793d9a9dd09df2427e711631
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-08-03 15:59:03 +0300

    UnitTesting

---
+++ Lab/Tests/AxeTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            //Arange
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            //Act
+            axe.Attack(dummy);
+
+            //Assert
+            Assert.AreEqual(9, axe.DurabilityPoints, "Axe Durability doesn't change after attack");
+        }
+    }
+}
